d3f4748e194ae527cfa0a99d2dbb1e0c21b10116
John Huddleston <huddlej@gmail.com>
2024-07-14 18:59:32 -0700

    Bump to version 2.2.0
    Bumps version and drops the redundant version attribute from the
    top-level module, since importlib provides that information already.
    What we really need is a `--version` flag for each pathogen-embed
    command, but that is for a future PR.

--- src/pathogen_embed/__init__.py
+++ src/pathogen_embed/__init__.py
@@ -6,6 +6,5 @@ Reduced dimension embeddings for pathogen sequences.
 
 from .__main__ import make_parser_cluster, make_parser_distance, make_parser_embed
 
-__version__ = "2.1.0"
 __author__ = 'Sravani Nanduri, John Huddleston'
 __credits__ = 'Bedford Lab, Vaccine and Infectious Disease Division, Fred Hutchinson Cancer Center, Seattle, WA, USA'
